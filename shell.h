82b0423f2b06274d30631efd21f0662251983554
SAMKE-glitch <sammythemwa@gmail.com>
2023-04-22 22:53:00 -0700

    created tokenizer file and updated shell file

--- shell.h
+++ shell.h
@@ -11,7 +11,10 @@
 
 extern char **environ;
 
-
+void run_cd_command(char **token_arr, int token_index);
+void run_exit_command();
+void run_ls_command(char **token_arr, int status);
+void run_env_command();
 void tokenizer(char *comand, char *delimiter);
 void print_env(void);
 void get_command(char **comand, size_t *n);
---
+++ tokenizer.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * tokenizer - function that splits string of commands
+ * @comand: string to break
+ * @delimiter: character to use to break
+ * Return: Nothing
+ */
+void tokenizer(char *comand, char *delimiter)
+{
+	char *token;
+	int token_index = 0;
+	char *token_arr[100];
+	int status;
+	char **env = environ;
+
+	token = strtok(comand, delimiter);
+	while (token != NULL)
+	{
+		token_arr[token_index] = token;
+		token_index++;
+		token = strtok(NULL, delimiter);
+	} token_arr[token_index] = NULL;
+	if (strcmp(token_arr[0], "cd") == 0)
+	{
+		run_cd_command(token_arr, token_index);
+	} else if (strcmp(token_arr[0], "exit") == 0)
+	{
+		run_exit_command();
+	} else if (strcmp(token_arr[0], "ls") == 0)
+	{
+		run_ls_command(token_arr, status);
+	} else if (strcmp(token_arr[0], "env") == 0)
+		run_env_command();
+	else
+	{
+		pid_t child = fork();
+
+		if (child == -1)
+			exit(EXIT_FAILURE);
+		else if (child == 0)
+		{
+			execve(token_arr[0], token_arr, env);
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		} else
+		{
+			waitpid(child, &status, 0);
+		}
+	}
+}
